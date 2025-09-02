322186dce690d0b62705b4f00a97212b76e82cde
GRIZZLY1111 <msolihov28@gmail.com>
2025-09-02 14:03:48 +0500

    Add files via upload

---
+++ Solihovnew/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SolihovMD_PR-31_03"
+include(":app")
+ 
