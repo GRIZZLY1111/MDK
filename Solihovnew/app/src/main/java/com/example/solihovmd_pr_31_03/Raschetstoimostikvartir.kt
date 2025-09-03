d7e1c72ddce250a46002d62edac585a94e37193b
GRIZZLY1111 <msolihov28@gmail.com>
2025-09-03 08:12:34 +0300

    Add files via upload

---
+++ Solihovnew/app/src/main/java/com/example/solihovmd_pr_31_03/Raschetstoimostikvartir.kt
@@ -0,0 +1,11 @@
+package com.example.solihovmd_pr_31_03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Raschetstoimostikvartir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_raschetstoimostikvartir)
+    }
+}
